e665413a395cd598386510e3e6d9e6f466a311ec
devika <devika.pathiyil@bovlabs.com>
2025-06-16 19:24:38 +0530

    adding project

---
+++ Project2-AI-Agent-crewai/devops_crewai_project/src/devops_crewai_project/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from devops_crewai_project.crew import DevopsCrewaiProject
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'Best investment plans for 2025',
+        'current_year': str(datetime.now().year)
+    }
+    
+    try:
+        DevopsCrewaiProject().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "Best investment plans for 2025",
+        'current_year': str(datetime.now().year)
+    }
+    try:
+        DevopsCrewaiProject().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        DevopsCrewaiProject().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "Best investment plans for 2025",
+        "current_year": str(datetime.now().year)
+    }
+    
+    try:
+        DevopsCrewaiProject().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
